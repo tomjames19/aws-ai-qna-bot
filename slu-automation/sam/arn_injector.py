73c2f4565486a8b2cd80d5fa5c3e73ad53dd245e
EC2 Default User <ec2-user@ip-172-31-34-230.ec2.internal>
2019-09-12 20:15:55 +0000

    updated arninjector and lambda json doucument

--- slu-automation/sam/arn_injector.py
+++ slu-automation/sam/arn_injector.py
@@ -2,18 +2,18 @@ import json
 import sys
 import re
 
-#hydrating qna_lambda.json
+#hydrates qna_lambda.json
 def jsonInjector(feedback, sodexo, bus, allergen):
     with open('qna_lambda.json', 'r+') as f:
         data = json.load(f)
         for item in data['qna']:
-            if 'Feedback' in item['qid']:
+            if 'Feedback' in item['qid'] and item['l']:
                 item['l'] = feedback[1:-1]
-            elif 'sodexo' in item['qid']:
+            elif 'sodexo' in item['qid'] and item['l']:
                 item['l'] = sodexo[1:-1]
-            elif 'bus.' in item['qid']:
+            elif 'bus.' in item['qid'] and item['l']:
                 item['l'] = bus[1:-1]
-            elif 'grand.' in item['qid']:
+            elif 'grand.' in item['qid'] and item['l']:
                 item['l'] = allergen[1:-1]
         f.seek(0)
         json.dump(data, f, indent=4)
