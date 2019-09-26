88f0cbd87b8340365806ac79154f9ac41ea5150a
Mohamed khalil <khalilmd@amazon.com>
2019-09-26 18:29:12 +0000

    added sports schedule and results lambda hook

---
+++ templates/examples/extensions/py_lambda_hooks/SluSportsResults/SluSportsResults.py
@@ -0,0 +1,64 @@
+import json
+import qnalib 
+import feedparser
+
+def lambda_handler(event, context):
+    event_results = event["res"]["result"]
+    sport_type  = event_results["args"][0]
+    team_endpoint = feedparser.parse('http://www.slubillikens.com/rss.dbml?db_oem_id=27200&media=results&RSS_SPORT_ID={}'.format(sport_type))
+    print(team_endpoint)
+    if team_endpoint.get('feed') and team_endpoint.get('entries'):
+        game_information = team_endpoint['entries'][0]['title']
+        
+    
+    
+    
+        outcome = game_information[-7]    
+        if outcome == 'L':
+            game_response = game_information.replace('L','Lost')
+        elif outcome == 'W':
+            game_response = game_information.replace('W','Won')
+        elif outcome == 'T':
+            game_response = game_information.replace('T','Tied')
+        else:
+            qnalib.ssml_response(event,spoken_response(game_information))
+            event['res']['message'] = "I am Unable to retreive the results for the scores right now. Please visit slubillikens.com for results on all sporting events."
+            return event
+
+        
+        qnalib.markdown_response(event,game_response)
+        qnalib.ssml_response(event,spoken_response(game_information))
+        qnalib.text_response(event,game_response)
+        
+
+        
+    else:
+
+        event['res']['message'] = "I am Unable to retreive the results for the scores right now. Please visit slubillikens.com for results on all sporting events."
+
+    return event
+        
+def spoken_response(result):
+    team_names = result[:result.find('(') -1]
+    slu_score = result[-4]
+    slu_name = team_names.split('vs')[:-2]
+    opponent_score = result[-2]
+    opponent_name = team_names.split('vs')[1]
+    if result[-7] == 'L':
+        outcome = 'Lost'
+    elif result[-7] == 'W':
+        outcome = 'Won'
+    elif result[-7] == 'T':
+        outcome = 'Tied'
+    else:
+        outcome = None
+
+    
+    if outcome == 'Won' or outcome == 'Tied':
+        alexa_response = "saint louis university {} {} against {} with the a score of {} to {}".format(slu_name,outcome,opponent_name,slu_score,opponent_score)
+    elif outcome == "Lost":
+        alexa_response = "saint louis university {} {} against {} with a score of {} to {}".format(slu_name,outcome,opponent_name,opponent_score,slu_score)
+    else:
+        alexa_response = "I am Unable to retreive the results for the scores right now. Please visit slubillikens.com for results on all sporting events."
+    
+    return alexa_response
---
+++ templates/examples/extensions/py_lambda_hooks/SluSportsSchedule/SluSportsSchedule.py
@@ -0,0 +1,39 @@
+import json
+import qnalib 
+import feedparser
+import datetime
+import calendar
+
+def lambda_handler(event, context):
+    event_results = event["res"]["result"]
+    sport_type  = event_results["args"][0]
+    team_endpoint = feedparser.parse('http://www.slubillikens.com/rss.dbml?db_oem_id=27200&media=schedulesxml&RSS_SPORT_ID={}'.format(sport_type))
+    print(team_endpoint)
+    if team_endpoint.get('feed') and team_endpoint.get('entries') :
+        return_message = date_conversion(team_endpoint)
+        qnalib.markdown_response(event,return_message) 
+        qnalib.text_response(event,return_message)
+        qnalib.ssml_response(event,return_message)
+    
+        return event
+        
+        
+    else:
+        event['res']['message'] = "Cannot locate schedule at this time. Please visit slubillikens.com for full sports schedule information."
+        return event
+        
+        
+def date_conversion(game_date):
+    scheduled_game = game_date['entries'][0]
+    date_split = scheduled_game['date'].split()
+    team_name = scheduled_game['sport']
+    opponent_name = scheduled_game['opponent']
+    date_of_game = date_split[0]
+    time_of_game = date_split[1] + date_split[2]
+    converted_date = datetime.datetime.strptime(date_of_game, '%m/%d/%Y')
+    day_of_week = converted_date.weekday()
+    days_of_week_dict = dict(zip(range(7),calendar.day_name))
+
+    response = 'The {} game vs {} is on {} {} at {}'.format(team_name,opponent_name,days_of_week_dict[day_of_week], date_of_game, time_of_game)
+    return response
+    
