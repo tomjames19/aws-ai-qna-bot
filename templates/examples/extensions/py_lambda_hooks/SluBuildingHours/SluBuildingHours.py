e016115c0fb949ed449f26800379225a1306968e
Mohamed khalil <khalilmd@amazon.com>
2019-10-17 15:30:46 +0000

    updated building hours skill

--- templates/examples/extensions/py_lambda_hooks/SluBuildingHours/SluBuildingHours.py
+++ templates/examples/extensions/py_lambda_hooks/SluBuildingHours/SluBuildingHours.py
@@ -14,7 +14,8 @@ from pytz import timezone
 
 
 def handler(event, context):
-    
+    print("start of new function ##########################################")
+    print(json.dumps(event))
     response = ''
     event_results = event["res"]["result"]
     building_arg = event_results["args"][0]
@@ -29,67 +30,80 @@ def handler(event, context):
     
     if(dayOfWeek == None): 
         dayOfWeek = todaysDay
+    elif(dayOfWeek == "Tomorrow"):
+        dayOfWeek = calendar.day_name[(central_time + datetime.timedelta(days=1)).weekday()]
     
 
     
-
-       
-    
-    #time_delta = 7 - todays_int + desired_day
-    #print(time_delta)
-    
-   # - Extracting the building name from the event args (This is set in the content designer, and must match case)
+    # - Extracting the building name from the event args (This is set in the content designer, and must match case)
     buildingName = event["res"]["result"]["args"][0]
     
     # - Query DynamoDB for the stop ID, which we will use to query the doublemap API.
     buildingID = getBuildingIDfromName(buildingName)
-    thursday_time = day_to_dt_converter(todaysDay,dayOfWeek,central_time)
-    scheduleID = getScheduleIDfromBuildingID(buildingID,thursday_time)
-    daysfromSchedule = getDaysfromScheduleID(scheduleID,dayOfWeek)
-    
-    print(thursday_time)
+    day_in_dt = day_to_dt_converter(todaysDay,dayOfWeek,central_time)
+    scheduleID = getScheduleIDfromBuildingID(buildingID,day_in_dt)
+    day_query = getDaysfromScheduleID(scheduleID,dayOfWeek)
+    if isinstance(day_query, list):
+        queried_day = day_query[0]
+    else:
+        queried_day = day_query
+        
 
+        
+    
+    if queried_day == "unavailable" and todaysDay == dayOfWeek:
+        initialmessage = "I cant find the hours for the {} building today.\n".format(buildingName)
+    elif queried_day == "unavailable":
+        initialmessage = "I cant find the hours for the {} building on {}. \n".format(buildingName,dayOfWeek)
+        
+    elif(queried_day['isclosed'] and todaysDay == dayOfWeek):
+        initialmessage = "Today the {} is closed\n ".format(buildingName)
+        
+    elif queried_day['isclosed']:
+        initialmessage = "On {} the {} is closed\n ".format(str(expandDayfromShortName(queried_day['days'])), buildingName)
+        
+    elif todaysDay == dayOfWeek:
+        initialmessage = "Today the {} is open from {} to {}\n".format(buildingName, dt.strptime(str(queried_day['open']), '%H:%M').strftime('%I:%M %p'), dt.strptime(str(queried_day['closed']), '%H:%M').strftime('%I:%M %p'))
+    else:
+        initialmessage = "On {} the {} is open from {} to {}\n".format(str(expandDayfromShortName(queried_day['days'])), buildingName, dt.strptime(str(queried_day['open']), '%H:%M').strftime('%I:%M %p'), dt.strptime(str(queried_day['closed']), '%H:%M').strftime('%I:%M %p'))
     
     
     # - Initialize the markdown values.
-    markdown = "|   Day  |   Opens  |Closes |\n|:------------|:-----------------:|-------:|"
-    
-    # - Construct the days message.
-    for days in daysfromSchedule:
-        
-        # - Initialize the short response. 
-        if(expandDayfromShortName(days['days']) == dayOfWeek):
-            if(days['isclosed']):
-                initialmessage = "On {} the {} is closed\n ".format(str(expandDayfromShortName(days['days'])), buildingName)
-            else :
-                initialmessage = "On {} the {} is open from {} to {}\n".format(str(expandDayfromShortName(days['days'])), buildingName, dt.strptime(str(days['open']), '%H:%M').strftime('%I:%M %p'), dt.strptime(str(days['closed']), '%H:%M').strftime('%I:%M %p'))
+    markdown = ''
+    if event["req"]["_type"] != "ALEXA" and event["req"]["_event"]["requestAttributes"] == None:
+        weekday_dict = dict(zip(calendar.day_name,range(7)))
+        todays_number = weekday_dict[dayOfWeek]
+        for i in range(todays_number,todays_number + 7):
+            dt_object = day_to_dt_converter(todaysDay,i,central_time)
+            week_schedule_id = getScheduleIDfromBuildingID(buildingID,dt_object)
+            dt_weekday = calendar.day_name[dt_object.weekday()]
+            week_object = getDaysfromScheduleID(week_schedule_id,dt_weekday)
+            if isinstance(week_object, list):
+                if week_object and week_object[0]['isclosed']:
+                    markdown +=    "\n|    {0}      |  Closed      | Closed      |".format(dt_weekday)
+                else:
+                    markdown +=    "\n|    {0}      |  {1} &nbsp; &nbsp; &nbsp; &nbsp;    | {2}      |".format(dt_weekday, dt.strptime(str(week_object[0]['open']), '%H:%M').strftime('%I:%M %p'), dt.strptime(str(week_object[0]['closed']), '%H:%M').strftime('%I:%M %p'))
+        if markdown:
+            markdown = "|   Day  |   Opens  |Closes |\n|:------------|:-----------------:|-------:|" + markdown
 
-        
-        if days['days']:
-            if(days['isclosed']):
-                markdown +=    "\n|    {0}      |  Closed      | Closed      |".format(str(expandDayfromShortName(days['days'])))            
-            else:
-                markdown +=    "\n|    {0}      |  {1}      | {2}      |".format(str(expandDayfromShortName(days['days'])), dt.strptime(str(days['open']), '%H:%M').strftime('%I:%M %p'), dt.strptime(str(days['closed']), '%H:%M').strftime('%I:%M %p'))
-
-
-        response =  initialmessage 
+    response =  initialmessage 
     
     # - Set the response message in the event object, and return the event.
     
-    if response:
-        # markdown = initialmessage + " following are the hours for the week \n" +  markdown 
+    if response and markdown:
         markdown = "{} \n{}".format(initialmessage, markdown)
+        qnalib.markdown_response(event,markdown)
+    elif response:
         ssml = response 
         text = response 
-        qnalib.markdown_response(event,markdown) 
         qnalib.text_response(event,text)
         qnalib.ssml_response(event,ssml)
     
+    elif markdown:
+        qnalib.markdown_response(event,inactive_building_text) 
     else:
         inactive_building_text = "Building open hours can be found here http://slu.edu"
         inactive_schedule_ssml = "No buildings are open today"
-        
-        qnalib.markdown_response(event,inactive_building_text) 
         qnalib.text_response(event,inactive_building_text)
         qnalib.ssml_response(event,inactive_schedule_ssml)
         
@@ -101,47 +115,53 @@ def handler(event, context):
 
 def searchUtteranceforDoW(utterance):
     
-    patterns = [' monday ', ' mon ']
+    patterns = ['monday', ' mon ']
     
     for pattern in patterns:
         if re.search(pattern, utterance.lower()):
             return "Monday"
 
-    patterns = [' tuesday ', ' tue ']
+    patterns = ['tuesday', ' tue ']
 
     for pattern in patterns:
         if re.search(pattern, utterance.lower()):
             return "Tuesday"
 
-    patterns = [' wednesday ', ' wed ']
+    patterns = ['wednesday', ' wed ']
 
     for pattern in patterns:
         if re.search(pattern, utterance.lower()):
             return "Wednesday"
 
-    patterns = [' thursday ', ' thu ']
+    patterns = ['thursday', ' thu ']
 
     for pattern in patterns:
         if re.search(pattern, utterance.lower()):
             return "Thursday"
 
-    patterns = [' Friday ', ' fri' ]
+    patterns = ['friday', ' fri' ]
 
     for pattern in patterns:
         if re.search(pattern, utterance.lower()):
             return "Friday"
 
-    patterns = [' Saturday ', ' sat ']
+    patterns = ['saturday', ' sat ']
 
     for pattern in patterns:
         if re.search(pattern, utterance.lower()):
             return "Saturday"
             
-    patterns = [' Sunday ', 'sun']
+    patterns = ['Sunday', 'sun']
 
     for pattern in patterns:
         if re.search(pattern, utterance.lower()):
             return "Sunday"
+            
+    patterns = ['tomorrow']
+    
+    for pattern in patterns:
+        if re.search(pattern, utterance.lower()):
+            return "Tomorrow"
 
     return None
 
@@ -150,7 +170,9 @@ def searchUtteranceforDoW(utterance):
     
 # - Pull the Daily Schedule 
 def getDaysfromScheduleID(scheduleID,dayOfWeek):
-
+    
+    if scheduleID == "unavailable":
+        return "unavailable"
     table = boto3.resource('dynamodb').Table(os.environ['DAY'])
     daysarray =[]
     
@@ -159,7 +181,7 @@ def getDaysfromScheduleID(scheduleID,dayOfWeek):
         FilterExpression=Key('dayScheduleId').eq(scheduleID) & Key('day_name').eq(dayOfWeek[:3])
  
         )
-    print(response)
+
 
     try:
         for i in response['Items']:
@@ -175,15 +197,13 @@ def getDaysfromScheduleID(scheduleID,dayOfWeek):
             
             # Sort the days by their order in the DB.
             dayssorted  = sorted(daysarray, key=lambda days: days['order'])
+            return dayssorted
             
   
     except IndexError:
-        scheduleID = 'unavailable'
-        
-    
- 
+        dayssorted = 'unavailable'
     # - Return the schedule ID.
-    return dayssorted
+        return dayssorted
 
 
 # Query the Schedule ID from the building.
@@ -191,7 +211,8 @@ def getScheduleIDfromBuildingID(buildingID,current_date):
 
    # print (buildingID)
     current_date = current_date.replace(hour=0, minute=0, second=0, microsecond=0)
-    print(current_date)
+    
+
 
     
     table = boto3.resource('dynamodb').Table(os.environ['SCHEDULE'])
@@ -200,30 +221,37 @@ def getScheduleIDfromBuildingID(buildingID,current_date):
     response = table.scan(
         FilterExpression=Key('scheduleBuildingId').eq(buildingID)
         )
-    print(json.dumps(response))
+ 
     
 
-    
+    special_schedule_id = ''
+    scheduleID = ''
+    nonschedule = ''
     for i in response["Items"]:
+ 
         central_object = timezone('US/Central')
         end_date = central_object.localize(dt.strptime(i["end_date"], '%a %b %d %Y'))
+
         start_date = central_object.localize(dt.strptime(i["start_date"], '%a %b %d %Y'))
+
+        if i["default_schedule"] == False:
+            if start_date <= current_date <= end_date:
+                 special_schedule_id = i["id"]
+                 
         if start_date <= current_date <= end_date:
             scheduleID = i["id"]
-            return scheduleID
-
+            
         else:
-            scheduleID = 'unavailable'
-    return scheduleID
+            nonschedule = 'unavailable'
 
-    try:
-        scheduleID = response['Items'][0]['id']
-    except IndexError:
-        scheduleID = 'unavailable'
-        
+    if special_schedule_id:
+        return special_schedule_id
+    elif scheduleID:
+        return scheduleID
+    else:
+        return nonschedule
     
-    # - Return the schedule ID.
-    return scheduleID
+
 
 
 # - Parse the building ID from the arg name.
@@ -264,8 +292,10 @@ def expandDayfromShortName(dayName):
 def day_to_dt_converter(todaysDay,dayOfWeek,central_time):
     weekday_dict =dict(zip(calendar.day_name,range(7)))
     todays_int = weekday_dict[todaysDay]
-    desired_day = (weekday_dict[dayOfWeek])
-
+    if isinstance(dayOfWeek, str):
+        desired_day = (weekday_dict[dayOfWeek])
+    else:
+        desired_day = dayOfWeek
     
     if todays_int == 0 and todays_int != desired_day:
         exact_day = central_time + datetime.timedelta(days=desired_day)
